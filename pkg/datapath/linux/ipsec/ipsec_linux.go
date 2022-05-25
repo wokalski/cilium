76a5bb907c2418050598fde28046fb4396507f9d
Gilberto Bertin <jibi@cilium.io>
2022-05-25 11:54:43 +0200

    logfields: add SPI and OldSPI IPSec constants
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -287,7 +287,7 @@ func ipSecReplacePolicyOut(src, dst, tmplSrc, tmplDst *net.IPNet, dir IPSecDir) 
 }
 
 func ipsecDeleteXfrmStatesNotMatchingSPI(spi uint8) {
-	scopedLog := log.WithField("spi", spi)
+	scopedLog := log.WithField(logfields.SPI, spi)
 
 	xfrmStateList, err := netlink.XfrmStateList(0)
 	if err != nil {
@@ -300,7 +300,7 @@ func ipsecDeleteXfrmStatesNotMatchingSPI(spi uint8) {
 			continue
 		}
 
-		scopedLog := scopedLog.WithField("oldSPI", s.Spi)
+		scopedLog := scopedLog.WithField(logfields.OldSPI, s.Spi)
 
 		scopedLog.Info("Deleting stale XFRM state")
 		if err := netlink.XfrmStateDel(&s); err != nil {
@@ -329,7 +329,7 @@ func ipsecDeleteXfrmState(ip net.IP) {
 }
 
 func ipsecDeleteXfrmPoliciesNotMatchingSPI(spi uint8) {
-	scopedLog := log.WithField("spi", spi)
+	scopedLog := log.WithField(logfields.SPI, spi)
 
 	xfrmPolicyList, err := netlink.XfrmPolicyList(0)
 	if err != nil {
@@ -347,7 +347,7 @@ func ipsecDeleteXfrmPoliciesNotMatchingSPI(spi uint8) {
 			continue
 		}
 
-		scopedLog := scopedLog.WithField("oldSPI", policySPI)
+		scopedLog := scopedLog.WithField(logfields.OldSPI, policySPI)
 
 		scopedLog.Info("Deleting stale XFRM policy")
 		if err := netlink.XfrmPolicyDel(&p); err != nil {
@@ -763,7 +763,8 @@ func doReclaimStaleKeys() {
 
 	xfrmStateList, err := netlink.XfrmStateList(0)
 	if err != nil {
-		log.WithField("spi", ipSecCurrentKeySPI).WithError(err).Warning("Failed to list XFRM states")
+		log.WithField(logfields.SPI, ipSecCurrentKeySPI).
+			WithError(err).Warning("Failed to list XFRM states")
 		return
 	}
 
